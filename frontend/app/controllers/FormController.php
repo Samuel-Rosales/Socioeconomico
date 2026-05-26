88fe4b07a6e36b8294cd50b47f109fd55ad5b4ce
Edgarbri <edgardev26@gmail.com>
2026-05-26 16:16:04 -0400

    Fix data retrieval in cargarCatalogos method to access nested data structure

--- frontend/app/controllers/FormController.php
+++ frontend/app/controllers/FormController.php
@@ -86,8 +86,8 @@ class FormController extends Controller
     {
         try {
             $response = $this->apiService->get('/catalogo/all');
-            if ($response['success'] && isset($response['data'])) {
-                return $response['data'];
+            if ($response['success'] && isset($response['data']['data'])) {
+                return $response['data']['data'];
             }
         } catch (\Exception $e) {
         }
